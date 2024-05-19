0f72234f3b6be72c3f46041b36420d603466c8fb
VitaBrrevis <vitalina.doronina@gmail.com>
2024-05-19 12:41:37 +0300

    added tabindexes, changed logic for lookbook variants, fixed bug with default cart drawer and altered single option tag

--- assets/cart-drawer.js
+++ assets/cart-drawer.js
@@ -50,7 +50,9 @@ class CartDrawer extends HTMLElement {
   close() {
     this.classList.remove('active');
     removeTrapFocus(this.activeElement);
-    document.body.classList.remove('overflow-hidden');
+    if (!document.querySelector('.product-popup-modal[open]')){
+       document.body.classList.remove('overflow-hidden');
+    }
   }
 
   setSummaryAccessibility(cartDrawerNote) {
